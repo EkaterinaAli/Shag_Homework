ae57ada421088ae5a3e4c5314cc60059bdc07999
EkaterinaAli <105931947+EkaterinaAli@users.noreply.github.com>
2022-10-23 11:25:35 +0300

    Add files via upload

--- homework24/BoxStorageTest.java
+++ homework24/BoxStorageTest.java
@@ -1,6 +1,8 @@
 package homework24;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 
 public class BoxStorageTest {
@@ -12,7 +14,7 @@ public class BoxStorageTest {
         //Поместить объекты в очередь с помощью метода offer().
         //Удалить объекты методом poll().
         Queue<HeavyBox> boxQueue = new ArrayDeque<>();
-        Queue<HeavyBox> heavyBoxes = new ArrayDeque<>();
+
         boxQueue.offer(new HeavyBox(10));
         boxQueue.offer(new HeavyBox(5));
         boxQueue.offer(new HeavyBox(20));
@@ -20,5 +22,28 @@ public class BoxStorageTest {
         System.out.println("Сейчас в очереди добавлено " + boxQueue.size() + " коробок.");
         System.out.println(boxQueue.poll() + " убирается");
         System.out.println("Сейчас в очереди " + boxQueue.size() + " коробки.");
+        // 6. Разделение коллекции
+        //Создать коллекцию, содержащую объекты HeavyBox.
+        // Написать метод, который перебирает элементы коллекции и проверяет вес коробок.
+        // Если вес коробки больше 300 гр, коробка перемещается в другую коллекцию.
+        List<HeavyBox> boxSmall = new ArrayList<>();
+        List <HeavyBox> boxBig = new ArrayList<>();
+        boxSmall.add(new HeavyBox(500));
+        boxSmall.add(new HeavyBox(200));
+        boxSmall.add(new HeavyBox(100));
+        boxSmall.add(new HeavyBox(900));
+        System.out.println("Коробки до сортировки " + boxSmall);
+        sortBox(boxSmall,boxBig);
+        System.out.println("Коробки после сортирвки " + boxBig);
+    }
+    public static void sortBox (List <HeavyBox> boxSmall, List<HeavyBox> boxBig) {
+
+        for (HeavyBox boxes : boxSmall) {
+            if (boxes.getWeight() > 300 ){
+                boxBig.add(boxes);
+            }
+            boxSmall.remove(boxBig);
+        }
     }
 }
+
